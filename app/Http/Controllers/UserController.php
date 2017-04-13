3286c6d78f3dcb2d1dd9b5de4438fc2754ac0c8c
inizio <digit4free@gmail.com>
2017-04-13 10:31:55 +0800

    user with facade

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+}
