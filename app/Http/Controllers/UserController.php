697241f66881ba5ac19c607f80b3d69846ddefbd
inizio <digit4free@gmail.com>
2017-04-25 22:09:16 +0800

    user api with login system

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Http\Request;
+use GenTux\Jwt\JwtToken;
+use GenTux\Jwt\GetsJwtToken;
 
 class UserController extends Controller
 {
+    use GetsJwtToken;
+
     /**
      * Create a new controller instance.
      *
@@ -13,5 +18,61 @@ class UserController extends Controller
      */
     public function __construct() {
         parent::__construct();
+    }
+
+    /**
+     * Register user
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function create_user(Request $request, JwtToken $jwt) {
+        $form = $request->input();
+        $form['password'] = app('hash')->make($form['password']);
+
+        $payload = User::create($form);
+        $payload['exp'] = time() + 3600 * 24;
+
+        $token = $jwt->createToken($payload);
+        return (string) 'Bearer '.$token;
+    }
+
+    /**
+     * Retrieve user by itsc
+     *
+     * @param Request $request
+     * @param string $_itsc
+     * @return void
+     */
+    public function get_user_by_id(Request $request, $_itsc) {
+        return User::find($_itsc);
+    }
+
+    /**
+     * Update user information
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function update_user(Request $request) {
+        $form = $request->input();
+        unset($form['itsc']);
+        
+        $user = User::find($this->jwtPayload()['itsc']);
+        $user->fill($form);
+        $user->save();
+
+        return User::find($this->jwtPayload()['itsc']);
+    }
+
+    public function delete_user(Request $request) {
+        $user = User::find($this->jwtPayload()['itsc']);
+
+        if (isset($user)) {
+            $user->delete();
+            return response(['success' => 'true']);
+        }
+
+        abort(403, 'Entry not exist');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,47 @@
 //   'uses' => 'UserController@all'
 // ]);
 
-// Create task
-$app->post('task', [
-  'uses' => 'TaskController@create_task'
+// Register user
+$app->post('user', [
+  'uses' => 'UserController@create_user'
 ]);
 
-$app->get('task', [
-  'uses' => 'TaskController@get_tasks'
+// Login to get token
+$app->post('login', [
+  'uses' => 'TokenController@login'
 ]);
 
-// Retrieve task by id
-$app->get('task/{_id}', [
-  'uses' => 'TaskController@get_tasks_by_id'
-]);
+$app->group(['middleware' => 'jwt'], function($app) {
+  // Retrieve user
+  $app->get('user/{_itsc}', [
+    'uses' => 'UserController@get_user_by_id'
+  ]);
 
-$app->patch('task/{_id}', [
-  'uses' => 'TaskController@update_tasks'
-]);
+  // Update user information
+  $app->put('user', [
+    'uses' => 'UserController@update_user'
+  ]);
+
+  // Delete user
+  $app->delete('user', [
+    'uses' => 'UserController@delete_user'
+  ]);
+  
+  // Create task
+  $app->post('task', [
+    'uses' => 'TaskController@create_task'
+  ]);
+
+  $app->get('task', [
+    'uses' => 'TaskController@get_tasks'
+  ]);
+
+  // Retrieve task by id
+  $app->get('task/{_id}', [
+    'uses' => 'TaskController@get_tasks_by_id'
+  ]);
+
+  $app->put('task/{_id}', [
+    'uses' => 'TaskController@update_tasks'
+  ]);
+});
